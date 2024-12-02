0ebe8065875b710ea48f89168b13f61ce3c81b8c
Jaypatel139 <54871480+pateljay1397@users.noreply.github.com>
2024-12-02 11:05:34 -0500

    Intial commit with home  and house components

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,11 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { HousingLocationComponent } from '../housing-location/housing-location.component';
+
+@Component({
+  selector: 'app-home',
+  imports: [CommonModule, HousingLocationComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css',
+})
+export class HomeComponent {}
